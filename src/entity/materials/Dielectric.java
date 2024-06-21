a0ac10d791aebb85080b4fcd66e35afe2bb5a3f5
shenxilong2000 <1097166568@qq.com>
2024-06-21 16:32:56 +0800

    1) Schlick 近似值

--- src/entity/materials/Dielectric.java
+++ src/entity/materials/Dielectric.java
@@ -1,6 +1,7 @@
 package entity.materials;
 
 import entity.*;
+import utils.CommonUtils;
 
 /**
  * 透明材质
@@ -12,6 +13,13 @@ public class Dielectric extends Material {
     // 封闭介质的折射率
 
     private final double refractionIndex;
+
+    private static double reflectance(double cosine, double refractionIndex) {
+        // Use Schlick's approximation for reflectance
+        double r0 = (1 - refractionIndex) / (1 + refractionIndex);
+        r0 = r0*r0;
+        return r0 + (1 - r0) * Math.pow((1 - cosine), 5);
+    }
 
     public Dielectric(double refractionIndex) {
         this.refractionIndex = refractionIndex;
@@ -28,7 +36,7 @@ public class Dielectric extends Material {
         boolean cannotRefract = ri * sinTheta > 1.0;
         Vec3 direction;
 
-        if (cannotRefract) {
+        if (cannotRefract || reflectance(cosTheta, ri) > CommonUtils.randomDouble()) {
             direction = Vec3.reflect(unitDirection, record.normal);
         } else {
             direction = Vec3.refract(unitDirection, record.normal, ri);
